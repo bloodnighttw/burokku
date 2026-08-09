430b8f200be940a834f694bab25b34f84a3e6a6b
bloodnighttw <bbeenn1227@gmail.com>
2026-08-09 14:45:58 +0800

    chore: remove window event plugin.

--- crates/runtime/src/plugins/scripts/window_event_keyboard_setup.js
+++
@@ -1,3 +0,0 @@
-globalThis.__burokku_dispatch_event = event => {
-    globalThis.lastEvent = event;
-};
--- crates/runtime/src/plugins/scripts/window_events_setup.js
+++
@@ -1,2 +0,0 @@
-globalThis.events = [];
-globalThis.__burokku_dispatch_event = event => events.push(event);
