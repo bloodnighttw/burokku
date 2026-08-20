078cb713d61ae1f2a5c22bf555622f2313727e2f
bloodnighttw <bbeenn1227@gmail.com>
2026-08-20 14:21:07 +0800

    chore: start to refactor dom implement.

--- crates/burokku/src/lib.rs
+++ crates/burokku/src/lib.rs
@@ -7,12 +7,9 @@
 //! [`BurokkuBuilder::runtime_plugin`], while latency-sensitive plugins can be
 //! installed in MTS with [`BurokkuBuilder::main_runtime_plugin`].
 
-mod app;
 #[cfg(debug_assertions)]
 pub mod debug;
 pub mod ui;
 
-pub use app::{Burokku, BurokkuBuilder, Error, Result, RunMode};
 pub use runtime;
 pub use runtime::{DualRuntime, DualRuntimeBuilder, Plugin};
-pub use ui::js_bridge::DomPlugin as RuntimePlugin;
--- crates/burokku/src/ui.rs
+++ crates/burokku/src/ui.rs
@@ -1,5 +1,1 @@
-pub mod computed;
 pub mod elements;
-pub mod events;
-pub mod frame;
-pub mod js_bridge;
